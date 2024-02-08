8a3c53dcff89487db9ab27c7dc487549ea9d9f00
ynori123 <norisama1159@gmail.com>
2024-02-09 00:21:57 +0900

    Refactor predict function to include actual rankings

--- predict.py
+++ predict.py
@@ -1,16 +1,17 @@
-def predict(best_ind, clf, data):
+def predict(best_ind, clf, data, y):
     # 新たなデータの取得
     # この部分は、新たなデータを取得する具体的な方法によります
     # ここでは、新たなデータをX_newとします
-    X_new = data.drop(['arrival', 'popularity'], axis=1)
-    # 新たなデータから最適な特徴量を選択
-    X_new_selected = X_new.iloc[:, [i for i, val in enumerate(best_ind) if val]]
+    # X_new = data.drop(['arrival', 'popularity'], axis=1)
+    # # 新たなデータから最適な特徴量を選択
+    # print(data)
+    X_new_selected = data.iloc[:, [i for i, val in enumerate(best_ind) if val]]
 
     # 新たなデータに対する予測を行う
     predictions = clf.predict(X_new_selected)
 
-    
     print("-"*30, "予測結果", "-"*30)
     data['予想順位'] = predictions
+    data['実際の順位'] = y
     print(data)
     return predictions
